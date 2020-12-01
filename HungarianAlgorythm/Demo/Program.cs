0d2a047106b4cccbb7a5c957e91a8248fd9f87ef
OmnixTry <Omnix.try@gmail.com>
2020-12-01 14:45:41 +0200

    Add costmatrix

---
+++ HungarianAlgorythm/Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using HungarianAlgorythm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[,] { { 2500, 4000, 3500}, { 4000, 6000, 3500}, { 2000, 4000, 2500} };
+            CostMatrix costMatrix = new CostMatrix(matrix);
+            costMatrix.Display();
+/*            Console.WriteLine();
+            costMatrix.SubstractFromColl(1, 500);            
+            costMatrix.Display();
+            Console.WriteLine();
+            costMatrix.SubstractFromRow(0, 500);
+            costMatrix.Display();
+            Console.WriteLine();*/
+
+            Console.WriteLine(costMatrix.GetMinOfTheRow(0));
+            Console.WriteLine(costMatrix.GetMinOfTheRow(1));
+            Console.WriteLine(costMatrix.GetMinOfTheRow(2));
+            Console.WriteLine();
+            Console.WriteLine(costMatrix.GetMinOfTheColl(0));
+            Console.WriteLine(costMatrix.GetMinOfTheColl(1));
+            Console.WriteLine(costMatrix.GetMinOfTheColl(2));
+
+
+
+
+
+        }
+    }
+}
---
+++ HungarianAlgorythm/HungarianAlgorythm/CostMatrix.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HungarianAlgorythm
+{
+    public class CostMatrix
+    {
+        public CostMatrix(int[,] matrix)
+        {
+            Matrix = matrix;
+            MarkedColls = new bool[matrix.GetLength(1)];
+            MarkedRows = new bool[matrix.GetLength(0)];
+        }
+
+        public int[,] Matrix { get; set; }
+        
+        public bool[] MarkedRows { get; } 
+        
+        public bool[] MarkedColls { get; }
+
+        public void SubstractFromRow(int rowNumber, int number)
+        {
+            int lengthOfRow = Matrix.GetLength(1);
+            for (int i = 0; i < lengthOfRow; i++)
+                Matrix[rowNumber, i] -= number;
+        }
+
+        public void SubstractFromColl(int collNumber, int number)
+        {
+            int lengthOfRow = Matrix.GetLength(0);
+            for (int i = 0; i < lengthOfRow; i++)
+                Matrix[i, collNumber] -= number;
+        }
+
+        public int GetMinOfTheRow(int rowNumber)
+        {
+            return Enumerable.Range(0, Matrix.GetLength(1))
+                .Select(n => Matrix[rowNumber, n])
+                .Min(x => x);
+        }
+
+        public int GetMinOfTheColl(int rowNumber)
+        {
+            return Enumerable.Range(0, Matrix.GetLength(0))
+                .Select(n => Matrix[n, rowNumber])
+                .Min(x => x);
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{Matrix[i, j]}".PadLeft(8));
+                }
+                Console.WriteLine();
+            }                
+        }
+    }
+}
