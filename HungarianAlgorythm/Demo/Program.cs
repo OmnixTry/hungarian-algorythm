4e32e6548b7b6ecb73d0adb3132d80eb8899c874
OmnixTry <Omnix.try@gmail.com>
2020-12-02 21:15:29 +0200

    add images

--- HungarianAlgorythm/Demo/Program.cs
+++ HungarianAlgorythm/Demo/Program.cs
@@ -10,27 +10,11 @@ namespace Demo
     class Program
     {
         static void Main(string[] args)
-        {
-            //int[,] matrix = new int[,] { { 2500, 4000, 3500}, { 4000, 6000, 3500}, { 2000, 4000, 2500} };
+        {               
             int[,] matrix = new int[,] { { 1500, 4000, 4500}, { 2000, 6000, 3500 }, { 2000, 4000, 2500 } };
             CostMatrix costMatrix = new CostMatrix(matrix);
             costMatrix.Display();
-            /*            Console.WriteLine();
-                        costMatrix.SubstractFromColl(1, 500);            
-                        costMatrix.Display();
-                        Console.WriteLine();
-                        costMatrix.SubstractFromRow(0, 500);
-                        costMatrix.Display();
-                        Console.WriteLine();*/
-            /*
-                        Console.WriteLine(costMatrix.GetMinOfTheRow(0));
-                        Console.WriteLine(costMatrix.GetMinOfTheRow(1));
-                        Console.WriteLine(costMatrix.GetMinOfTheRow(2));
-                        Console.WriteLine();
-                        Console.WriteLine(costMatrix.GetMinOfTheColl(0));
-                        Console.WriteLine(costMatrix.GetMinOfTheColl(1));
-                        Console.WriteLine(costMatrix.GetMinOfTheColl(2));*/
-
+            
             HungarianAlgoPerformer performer = new HungarianAlgoPerformer(costMatrix);
             performer.Execute();
             costMatrix.Display();
@@ -44,3 +28,5 @@ namespace Demo
         }
     }
 }
+// alternative start data
+//int[,] matrix = new int[,] { { 2500, 4000, 3500}, { 4000, 6000, 3500}, { 2000, 4000, 2500} };
